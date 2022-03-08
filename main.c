f8abb941c2b0a892d4616b230b3999466e86bd34
vitca64rus <vitca64rus@gmail.com>
2022-03-08 20:19:01 +0300

    start own term

---
+++ main.c
@@ -0,0 +1,55 @@
+#include <unistd.h>
+#include <term.h>
+#include <string.h>
+#include <stdio.h>
+#include "libft/libft.h"
+
+int	ft_putchar(int c)
+{
+	write(1, &c, 1);
+	return (0);
+}
+
+
+int	main(int argc, char **argv, char **env)
+{
+	char			str[200];
+	int				len;
+	struct termios	term;
+	char			*term_name = "xterm-256color"; //FIX_me for another term (env TERM=)
+
+	tcgetattr(0, &term);
+	term.c_lflag &= ~(ECHO);
+	term.c_lflag &= ~(ICANON);
+	tcsetattr(0, TCSANOW, &term);
+
+	tgetent(0, term_name);
+	tputs(save_cursor, 1, ft_putchar);
+	while (strcmp(str, "\4"))
+	{
+		do
+		{
+			len = read(0, str, 100);
+			if (!strcmp(str, "\e[A"))
+			{
+				tputs(restore_cursor, 1, ft_putchar);
+				tputs(tigetstr("ed"), 1, ft_putchar);
+				printf("previous\n");
+			}
+			else if (!strcmp(str, "\e[B"))
+			{
+				tputs(restore_cursor, 1, ft_putchar);
+				tputs(tigetstr("ed"), 1, ft_putchar); // FIX_ME - potential forbiden tigetstr
+				printf("next\n");
+			}
+			else if (!strcmp(str, key_backspace))
+			{
+				tputs(cursor_left, 1, ft_putchar);
+				tputs(tigetstr("ed"), 1, ft_putchar); // FIX_ME - potential forbiden tigetstr
+			}
+			else
+				write(1, str, len);
+		} while (strcmp(str, "\n") && strcmp(str, "\4"));
+	}
+    return(0);
+}
