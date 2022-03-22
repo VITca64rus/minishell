8094e3876186c5eabb4cbc568da9b394c26df900
vitca64rus <vitca64rus@gmail.com>
2022-03-22 13:16:54 +0300

    re-cut according to the operators (symbols: >, <, |).

--- ft_parsing.c
+++ ft_parsing.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_parsing.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: natalia <natalia@student.42.fr>            +#+  +:+       +#+        */
+/*   By: sazelda <sazelda@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/20 10:22:46 by natalia           #+#    #+#             */
-/*   Updated: 2022/03/21 21:34:26 by natalia          ###   ########.fr       */
+/*   Updated: 2022/03/22 13:15:50 by sazelda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@ void   	ft_parsing(char *str)
 	int	start;
 	t_tokens	*tokens=NULL;
 	t_tokens	*new;
+	t_tokens	*tmp;
 
 	count_dquotes = 0;
 	count_quotes = 0;
@@ -54,17 +55,49 @@ void   	ft_parsing(char *str)
 	}
 	else
 		printf("Error parsing");
-	
+	tmp = tokens;
 	while (tokens)
 	{
-		printf("%s\n", tokens->str);
+		i = 0;
+		if (tokens->str[0] != '\'' && tokens->str[0] != '"')
+		{
+			while (tokens->str[i] != '\0')
+			{
+				if ((tokens->str[i] == '|' || tokens->str[i] == '>' || tokens->str[i] == '<' ) && (i != 0))
+				{
+					new = (t_tokens *)malloc(sizeof(t_tokens));
+					new->str = ft_strdup(&tokens->str[i]);
+					tokens->str[i] = '\0';
+					new->next = tokens->next;
+					tokens->next = new;
+					break;
+				}
+				else if ((tokens->str[i] == '|' || tokens->str[i] == '>' || tokens->str[i] == '<' ) && (i == 0) && (tokens->str[i + 1] != '\0'))
+				{
+					new = (t_tokens *)malloc(sizeof(t_tokens));
+					new->str = ft_strdup(&tokens->str[i + 1]);
+					tokens->str[i + 1] = '\0';
+					new->next = tokens->next;
+					tokens->next = new;
+					break;
+				}
+				i++;
+			}
+		}
+		//printf("%s\n", tokens->str);
 		tokens = tokens->next;
+	}
+
+	while (tmp)
+	{
+		printf("%s\n", tmp->str);
+		tmp = tmp->next;
 	}
 }
 
 int main(void)
 {
-    char *str = "ls -l| \"echo\\\" pine apple\"";
+    char *str = "ls -l > file| \"echo\\\" pine | apple\"";
 	printf("TEST\n");
 	ft_parsing(str);
 	printf("STR = %s\n", str);
