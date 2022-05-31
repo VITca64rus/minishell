657c3d22121fef0bf6c74b5d89f0e57dd2879bfd
Hildegard Vayon <hvayon@oa-d5.msk.21-school.ru>
2022-05-31 21:23:02 +0300

    merge diff

--- ft_parsing.c
+++ ft_parsing.c
@@ -6,11 +6,7 @@
 /*   By: hvayon <hvayon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/20 10:22:46 by natalia           #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2022/04/17 13:42:37 by hvayon           ###   ########.fr       */
-=======
-/*   Updated: 2022/05/31 21:03:06 by hvayon           ###   ########.fr       */
->>>>>>> main
+/*   Updated: 2022/05/31 21:22:11 by hvayon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -238,11 +234,7 @@ int main(int argc, char **argv, char **envp)
 	// str = ft_strdup("< infile.txt cat | wc -l > outfile.txt");
 	str = ft_strdup("<< infile ls -l > file| echo 'pine | echo apple'");
 	data = ft_parsing(str);
-<<<<<<< HEAD
-	ft_search_command(data, envp);
-=======
 	lexical_analyzer(data);
->>>>>>> main
 	printf("STR = %s\n", str);
 }
 
--- minishell.h
+++ minishell.h
@@ -31,39 +31,9 @@ typedef struct s_data
 	struct s_data	*next;
 }	t_data;
 
-
-typedef struct s_redirs
-{
-	char *filename;
-	int mode;
-} t_redirs;
-
-typedef struct s_comnds
-{
-	t_redirs	**redirs;
-	t_list		*args;
-	//t_envars 	*envs;
-} t_comnds;
-
-// command parser
-
 void	ft_lstadd_back(t_tokens **lst, t_tokens *new);
 t_data	*ft_parse(char *str);
 void	ft_lstadd_back1(t_data **lst, t_data *new);
-<<<<<<< HEAD
-int		ft_pipex(t_info *store, char *here_doc_name, t_data *data, char **envp);
-void	child_process(int pipefd[2], char **cmd_args, t_pipex *data, char **envp);
-char	*ft_check_sistem_bin(char *cmd, char ***comands, char **paths);
-char	**ft_comands_for_full_path(char *cmd, char **comands);
-void	parent_process(int pipefd[2], char **cmd2, t_pipex *data, char **envp);
-void	ft_check_comand(char **cmd1, char ***comands, \
-						char **path, t_pipex **data);
-t_comnds	**ft_search_command(t_data *data, char **envp);
-void	execute_bin(t_data *data, char **comands, char **envp);
-int		ft_pipes_redirects(t_data *data);
-void	execute_command(t_data *data, char **commands, char **envp);
-=======
 void lexical_analyzer(t_data *data);
->>>>>>> main
 
 #endif
